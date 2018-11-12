14cb15799e5fbb907571300dc3b6cbaee4c40437
krishnam raju kollu <>
2018-11-12 12:07:12 +0530

    updated

---
+++ src/app/shared/components/twitter-share/twitter-share.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, AfterViewInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'tweet',
+  template: `<a href="https://twitter.com/share" [attr.data-text]="text" [attr.data-url]="url" class="twitter-share-button"></a>`
+})
+
+export class TwitterShareComponent implements AfterViewInit {
+  @Input() url  ;
+  @Input() text = '';
+
+  constructor() {
+    // load twitter sdk if required
+    const url = 'https://platform.twitter.com/widgets.js';
+    if (!document.querySelector(`script[src='${url}']`)) {
+      let script = document.createElement('script');
+      script.src = url;
+      document.body.appendChild(script);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    // render tweet button
+    window['twttr'] && window['twttr'].widgets.load();
+  }
+
+}
